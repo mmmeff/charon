a9869042637586332f148709b6acd72dd5920a56
Matt <matthewfrey118@gmail.com>
2026-06-29 17:49:24 -0600

    Revert "Install Vercel Web Analytics"

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Analytics } from "@vercel/analytics/react";
 import App from "./App";
 import { AppErrorBoundary } from "./components/AppErrorBoundary";
 import "./styles.css";
@@ -10,7 +9,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AppErrorBoundary>
       <App />
-      <Analytics />
     </AppErrorBoundary>
   </React.StrictMode>
 );
